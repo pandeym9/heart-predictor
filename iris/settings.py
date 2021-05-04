06fd5322e037b6a1feb271406ec373abd9f980ce
pandeym9 <68601486+pandeym9@users.noreply.github.com>
2021-05-04 20:51:52 +0530

    Update settings.py

--- iris/settings.py
+++ iris/settings.py
@@ -31,7 +31,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','heartmonitoringsystem.herokuapp.com/']
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Application definition
