a3439d3596d8f53c161d408d7514ab0b43f1cd5d
Mayank Pandey <mayankpandey@Mayanks-MacBook-Air.local>
2021-05-04 12:51:28 +0530

    first commit

--- iris/settings.py
+++ iris/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*e-vifgtz8^38rhj*x%&-k_&vs+v*24g1r_+#6atve@scpwylo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','heartmonitoring.com']
+ALLOWED_HOSTS = ['127.0.0.1','heartmonitoringsystem.com']
 
 
 
